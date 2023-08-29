5c5fb9a1ece9281f3e9bb5d1edfd0c1a8658a34c
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-29 18:22:15 -0400

    Add item amount to cart on click

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -4,9 +4,26 @@ export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
-  const addToCart = (id) => {
-    console.log(`item ${id} added to cart`);
-  }
+  const addToCart = (product, id) => {
+    const newItem = { ...product, amount: 1 }
+    const cartItem = cart.find(item => {
+      return item.id === id;
+    });
+    if (cartItem) {
+      const newCart = [...cart].map((item) => {
+        if (item.id === id) {
+          return {...item, amount: cartItem.amount + 1};
+        }
+        else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    }
+    else {
+      setCart([...cart, newItem]);
+    }
+  };
   return <CartContext.Provider value={{ addToCart }}>{children}</CartContext.Provider>;
 };
 
