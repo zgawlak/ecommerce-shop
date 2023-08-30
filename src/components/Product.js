a3dbf4ac1a889be59107d1a79fae8803341d13b3
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-29 21:03:00 -0400

    Change font, change product component styles

--- src/components/Product.js
+++ src/components/Product.js
@@ -21,11 +21,11 @@ const Product = ({ product }) => {
 
         <div className="absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
           <button onClick={() => addToCart(product, id)}>
-            <div className="flex justify-center items-center text-white w-12 h-12 bg-red-500">
+            <div className="flex justify-center items-center text-white w-12 h-12 bg-red-900">
               <BsPlus className="text-3xl" />
             </div>
           </button>
-          <Link to={`/product/${id}`} className="w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl">
+          <Link to={`/product/${id}`} className="w-12 h-12 bg-white flex justify-center items-center border-2">
             <BsEyeFill />
           </Link>
         </div>
@@ -37,7 +37,7 @@ const Product = ({ product }) => {
       <Link to={`/product/${id}`}>
         <h2 className="font-semibold mb-1">{title}</h2>
       </Link>
-      <div className="font-semibold">{CADollar.format(price)}</div>
+      <div className="font-medium text-red-900">{CADollar.format(price)}</div>
     </div>
   </div>;
 };
