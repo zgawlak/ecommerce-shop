ae2a619d5d6c28060ce41f4302f79aa61fbe54a4
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-09-01 22:31:31 -0400

    Create hero styles and text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import WomanImg from '../img/woman-hero.png';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="bg-pink-200 h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
+      <div className="container mx-auto flex justify-around h-full">
+        <div className="flex flex-col justify-center">
+          <div className="font-semibold flex items-center uppercase">
+            <div className="w-10 h-[2px] bg-red-900 mr-3"></div>NEW 2023
+          </div>
+          <h1 className="text-[70px] leading-[1.1] font-light mb-4">
+            HOME & FASHION<br />
+            <span className="font-semibold">AUTUMN</span>
+          </h1>
+          <Link to={'/'} className="self-start uppercase font-semibold underline decoration-2 underline-offset-4 hover:no-underline">
+            Discover More
+          </Link>
+        </div>
+        <div className="hidden lg:block">
+          <img src={WomanImg} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
